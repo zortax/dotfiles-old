71ce0d92a34f883358900d5afa1766b804f2a874
Leonard Seibold <leonard.seibold.m@gmail.com>
2018-11-25 15:59:00 +0100

    polybar

---
+++ polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Kill already running instances...
+killall -q polybar
+
+# Wait till process terminated...
+while pgrep -x polybar >/dev/null; do
+    sleep 1;
+done
+
+for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload top &
+done
+
+echo "Polybar started..."
+
---
+++ scripts/count-updates.sh
@@ -0,0 +1,10 @@
+!/bin/bash
+
+COUNT_ARCH=$(checkupdates | wc -l)
+COUNT_AUR=$(trizen -Su --aur --quiet | wc -l)
+TOTAL=$(( COUNT_ARCH + COUNT_AUR))
+
+if [[ $TOTAL -ge 1 ]]; then
+    echo "   $COUNT_ARCH / $COUNT_AUR "
+fi
+
