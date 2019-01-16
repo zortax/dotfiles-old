1099625d443b00e512c33f6387302e34af340c21
Leonard Seibold <leonard.seibold.m@gmail.com>
2019-01-16 13:53:05 +0100

    use checkupdates-aur instead of trizen

--- scripts/count-updates.sh
+++ scripts/count-updates.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 COUNT_ARCH=$(checkupdates | wc -l)
-COUNT_AUR=$(trizen -Su --aur --quiet | wc -l)
+COUNT_AUR=$(checkupdates-aur | wc -l)
 TOTAL=$(( COUNT_ARCH + COUNT_AUR))
 
 if [[ $TOTAL -ge 1 ]]; then
