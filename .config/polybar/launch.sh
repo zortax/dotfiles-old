c98afc5200d164e460a4002bf64614481173a5c2
Leonard Seibold <leo@zrtx.de>
2021-02-05 15:59:49 +0100

    update polybar, font size

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -15,22 +15,11 @@ MAIN_MONITOR="DP-0"
 for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     
     export MONITOR=$m 
-    export DESK_COUNT=$(bspc query -D -m $m | wc -l)
-    export DESK_VAR_WIDTH=$((DESK_COUNT * 32 + 10))
-    export DESK_OFFSET_NEXT=$((DESK_VAR_WIDTH + 16))
-
-    polybar --reload main -c ~/.config/polybar/workspace.ini &
 
     if [ "$m" = "$MAIN_MONITOR" ]; then
-        
-        polybar --reload main -c ~/.config/polybar/system.ini &
-        polybar --reload main -c ~/.config/polybar/apps.ini &
-        polybar --reload main -c ~/.config/polybar/updates.ini &
-
+        polybar --reload main -c ~/.config/polybar/main.ini &
     else
-
-        polybar --reload main -c ~/.config/polybar/time.ini &
-
+       polybar --reload secondary -c ~/.config/polybar/main.ini &
     fi
 
 done
