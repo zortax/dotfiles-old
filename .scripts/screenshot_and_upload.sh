bbbec219f081411827f525191ba7bc2eaf7ede8b
Leonard Seibold <leonard.seibold.m@gmail.com>
2022-12-25 22:28:30 +0100

    stuff

--- .scripts/screenshot_and_upload.sh
+++ .scripts/screenshot_and_upload.sh
@@ -2,11 +2,12 @@
 mkdir -p ~/Pictures/Screenshots
 touch ~/Pictures/Screenshots/imgur_upload_log
 FILE="$HOME/Pictures/Screenshots/$(date "+%F-%T").png"
-spectacle -rb --output $FILE
+#spectacle -rb --output $FILE
+maim -g $(slop) $FILE
 if [ ! -f $FILE ]; then
     exit 0
 fi
-response=$(curl -s -H "Authorization: Client-ID c9a6efb3d7932fd" -H "Expect: " -F "image=@$FILE" https://api.imgur.com/3/image.xml)
+response=$(curl -s -H "Authorization: Client-ID d010f9ca5c370bf" -H "Expect: " -F "image=@$FILE" https://api.imgur.com/3/image.xml)
 if [ $? -ne 0 ]; then
     notify-send "Screenshot taken\!" "Upload to Imgur failed..." -i image-x-generic
 elif echo "$response" | grep -q 'success="0"'; then
